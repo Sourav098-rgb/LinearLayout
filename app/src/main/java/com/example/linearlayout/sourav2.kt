7e3fa6385e7cfec91149cce8ab7cce51fb5e69dc
Sourav098-rgb <mishrasourav731@gmail.com>
2024-07-11 16:22:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/linearlayout/sourav2.kt
@@ -0,0 +1,28 @@
+package com.example.linearlayout
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class sourav2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sourav2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val buttonsave = findViewById<Button>(R.id.save)
+        val buttoncancle =findViewById<Button>(R.id.cancle)
+
+        buttonsave.setOnClickListener {  }
+        buttoncancle.setOnClickListener {  }
+
+
+    }
+}
