bad72a3464d075a8fb4a94c1e859cfd24264d7bc
Sourav098-rgb <mishrasourav731@gmail.com>
2024-07-12 17:08:24 +0530

    souravtwoscreen

--- app/src/main/java/com/example/linearlayout/sourav2.kt
+++ app/src/main/java/com/example/linearlayout/sourav2.kt
@@ -2,27 +2,63 @@ package com.example.linearlayout
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class sourav2 : AppCompatActivity() {
+class Sourav2 : AppCompatActivity() {
+    var btn:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sourav2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sourav)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val buttonsave = findViewById<Button>(R.id.save)
-        val buttoncancle =findViewById<Button>(R.id.cancle)
+        val btnsave=findViewById<Button>(R.id.btnsave)
+        val btncancle=findViewById<Button>(R.id.btncancle)
 
-        buttonsave.setOnClickListener {  }
-        buttoncancle.setOnClickListener {  }
-
+        btnsave.setOnClickListener {
+            Toast.makeText(this,"Saved",Toast.LENGTH_SHORT).show()
+        }
+        btncancle.setOnClickListener {
+            Toast.makeText(this,"Cancled",Toast.LENGTH_SHORT).show()
+        }
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"On Start",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"On Stop", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"On Restart", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"On Destroy", Toast.LENGTH_SHORT).show()
+
+    }
+
+
+
 }
